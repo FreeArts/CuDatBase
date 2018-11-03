d544dc22f498f3ded75684fd101342223508d34f
Peter Mogyorosi <freeartclub@gmail.com>
2018-11-03 12:47:36 +0100

    prepair source code

---
+++ cudatbase/src/select.h
@@ -0,0 +1,19 @@
+/*
+ * select.h
+ *
+ *  Created on: 2018 nov. 3
+ *      Author: freeart
+ */
+
+#ifndef SELECT_H_
+#define SELECT_H_
+
+#include "cuda_select.cuh"
+
+class SELECT {
+public:
+	SELECT();
+	virtual ~SELECT();
+};
+
+#endif /* SELECT_H_ */
